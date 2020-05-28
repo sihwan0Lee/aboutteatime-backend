3e140962d10f525487bdff7f504c5db12f8c967a
junePyo <junepyo.suh@gmail.com>
2020-05-28 13:57:02 +0900

    [Fix] Deleted utils.py - deleted utils.py just so that this branch contains only changes   related to user model

--- aboutteatime/settings.py
+++ aboutteatime/settings.py
@@ -27,7 +27,7 @@ HASH = my_settings.HASH['ALG']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders'
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
