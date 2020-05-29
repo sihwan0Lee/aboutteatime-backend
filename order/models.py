71ae4b1d0b77d0f26294d34da5ea7400e8e33ea9
sihwan0Lee <stay136@gmail.com>
2020-05-29 14:03:50 +0900

    [Fix]띄워쓰기 수정
    -필드명과 = 사이 띄워쓰기 간격조정

--- order/models.py
+++ order/models.py
@@ -3,15 +3,15 @@ from user.models import User
 from item.models import Item
 
 class Order(models.Model):
-	user					= models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
-	status					= models.OneToOneField('OrderStatus', on_delete=models.SET_NULL, null=True)
-	payment_method			= models.ForeignKey('PaymentMethod', on_delete=models.SET_NULL, null=True)
-	request					= models.CharField(max_length=500)
-	tracking_number			= models.CharField(max_length=200)
-	price					= models.DecimalField(max_digits=12, decimal_places=2)
-	cash_receipt_phone		= models.CharField(max_length=45, null=True, default=None)
-	escrow_policy			= models.BooleanField(default=False)
-	items					= models.ManyToManyField('Item', through='OrderItem') 
+	user				= models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+	status				= models.OneToOneField('OrderStatus', on_delete=models.SET_NULL, null=True)
+	payment_method		= models.ForeignKey('PaymentMethod', on_delete=models.SET_NULL, null=True)
+	request				= models.CharField(max_length=500)
+	tracking_number		= models.CharField(max_length=200)
+	price				= models.DecimalField(max_digits=12, decimal_places=2)
+	cash_receipt_phone	= models.CharField(max_length=45, null=True, default=None)
+	escrow_policy		= models.BooleanField(default=False)
+	items				= models.ManyToManyField('Item', through='OrderItem') 
 
 	def __str__(self):
 		return self.user
@@ -20,15 +20,15 @@ class Order(models.Model):
 		db_table = 'orders'
 
 class OrderItem(models.Model):
-	order					= models.ForeignKey('Order', on_delete=models.CASCADE)
-	item					= models.ForeignKey('Item', on_delete=models.CASCADE)
-	quantity				= models.IntegerField()
+	order	 = models.ForeignKey('Order', on_delete=models.CASCADE)
+	item	 = models.ForeignKey('Item', on_delete=models.CASCADE)
+	quantity = models.IntegerField()
 
 	class Meta:
 		db_table = 'order_items'
 
 class OrderStatus(models.Model):
-	status					= models.CharField(max_length=45)
+	status = models.CharField(max_length=45)
 
 	def __str__(self):
 		return self.status
@@ -37,7 +37,7 @@ class OrderStatus(models.Model):
 		db_table = 'order_status'
 
 class PaymentMethod(models.Model):
-	method					= models.CharField(max_length=45)
+	method = models.CharField(max_length=45)
 	
 	def __str__(self):
 		return self.method
