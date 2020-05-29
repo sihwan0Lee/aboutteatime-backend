b177a7129a5efddb97011d284cfed56db3322c14
junePyo <junepyo.suh@gmail.com>
2020-05-29 23:12:44 +0900

    [Fix] Deleted unused modules, modified User booleanfields - Instead of accessing by using dictionary keys, simply used "True"   expression; if key exists, the field value must be True

--- user/views.py
+++ user/views.py
@@ -6,11 +6,9 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.db import IntegrityError
-from django.db.models import Q
 
-from aboutteatime.utils import logindecorator
 from aboutteatime.settings import SECRET, HASH
-from user.models import User, UserStatus
+from user.models import User, UserGroup
 
 class SignUpView(View):
     def post(self, request):
@@ -26,10 +24,10 @@ class SignUpView(View):
                 phone             = data['phone'],
                 group             = UserGroup.objects.get(pk=1),
                 password          = bcrypt.hashpw(b_password, bcrypt.gensalt()),
-                privacy_3rd_party = data['privacy_3rd_party'] if data.get('privacy_3rd_party') else False,
-                privacy_foreign   = data['privacy_foreign'] if data.get('privacy_foreign') else False,
-                point_message     = data['point_message'] if data.get('point_message') else False,
-                web_message       = data['web_message'] if data.get('web_message') else False
+                privacy_3rd_party = True if data.get('privacy_3rd_party') else False,
+                privacy_foreign   = True if data.get('privacy_foreign') else False,
+                point_message     = True if data.get('point_message') else False,
+                web_message       = True if data.get('web_message') else False
             }
             return HttpResponse(status=200)
         except IntegrityError:
@@ -46,6 +44,7 @@ class SignInView(View):
                 if bcrypt.checkpw(data['password'].encode(‘utf-8’), user.password.encode(‘utf-8’)):
                     token = jwt.encode({'user_id':user.id}, SECRET, algorithm=HASH)
                     return JsonResponse({'token':token.decode('utf-8')}, status=200)
-            return JsonResponse({'error':'INCORRECT_ID_OR_PASSWORD'}, status=400)
+                return JsonResponse({'error': 'INCORRECT_PASSWORD'}, status=400)
+            return JsonResponse({'error':'INCORRECT_USERNAME'}, status=400)
         except KeyError:
             return JsonResponse({'error':'INVALID_KEY'}, status = 400)
