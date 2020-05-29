ea0a863771e845f6a9bd015e6ecc439aa948cd51
sihwan0Lee <stay136@gmail.com>
2020-05-29 15:43:25 +0900

    [Fix]수정
    -수정

--- order/models.py
+++ order/models.py
@@ -4,15 +4,18 @@ from item.models import Item
 
 class Order(models.Model):
 	user				= models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+	unregistered_user	= models.OneToOneField('UnregisteredUser', on_delete=models.CASCADE, default=None)
 	status				= models.OneToOneField('OrderStatus', on_delete=models.SET_NULL, null=True)
 	payment_method		= models.ForeignKey('PaymentMethod', on_delete=models.SET_NULL, null=True)
 	request				= models.CharField(max_length=500)
-	tracking_number		= models.CharField(max_length=200)
-	price				= models.DecimalField(max_digits=12, decimal_places=2)
+	tracking_number		= models.CharField(max_length=100)
+	final_price			= models.DecimalField(max_digits=12, decimal_places=2)
+	shipping_fee		= models.PositiveIntegerField(default=2500)
 	cash_receipt_phone	= models.CharField(max_length=45, null=True, default=None)
 	escrow_policy		= models.BooleanField(default=False)
 	items				= models.ManyToManyField('Item', through='OrderItem') 
-
+	ordered_date 		= models.DateTimeField(null=True)
+	
 	def __str__(self):
 		return self.user
 
@@ -22,7 +25,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
 	order	 = models.ForeignKey('Order', on_delete=models.CASCADE)
 	item	 = models.ForeignKey('Item', on_delete=models.CASCADE)
-	quantity = models.IntegerField()
+	quantity = models.PositiveIntegerField()
 
 	class Meta:
 		db_table = 'order_items'
