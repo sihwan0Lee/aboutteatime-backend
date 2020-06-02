28a7bf33faa88634b2a413b32a6cd6857725c69d
junePyo <junepyo.suh@gmail.com>
2020-06-02 23:39:50 +0900

    [Refactor] refactored ItemListView - using querystring, get_selected_items evaluates Q expressions and   filters all items, after which calls on select_sort() function to   return items list sorted according to the value of the sort parameter

--- item/views.py
+++ item/views.py
@@ -10,74 +10,65 @@ from django.db.models import Avg
 from item.models import Item
 
 class ItemListView(View):
-    OFFSET_PAGE = 24
+    ITEMS_IN_PAGE = 24
 
     def select_sort(self, sort, qs):
         if sort == 'review':
-            items = list(qs.annotate(count=Count('itemreview_set')).order_by('count'))
+            items = list(qs.annotate(count=Count('itemreview')).order_by('-count'))
         elif sort == 'popular':
-            items = list(qs.annotate(count=Count('order_set')).order_by('count'))
+            items = list(qs.annotate(count=Count('order')).order_by('-count'))
         elif sort == 'new_arrival':
-            items = list(qs.annotate(count=Count('id')).order_by('-count'))
+            items = list(qs.order_by('-id'))
         elif sort == 'price_desc':
-            items = list(qs.annotate(count=Count('price')).order_by('count'))
+            items = list(qs.order_by('-price'))
         elif sort == 'price_asc':
-            items = list(qs.annotate(count=Count('price')).order_by('-count'))
-        return items
-
-    def evaluate_items(self, sort, category, packs):
-        items = []
-        if packs[0] == 'all' and category == 'all':
-            items = self.select_sort(sort, Item.objects.all())
-            return items
-        if packs[0] == 'all' and category != 'all':
-            qs = Item.objects.filter(third_category__name = category)
-            items = self.select_sort(sort, qs)
-            return items
-        if packs[0] != 'all' and category == 'all':
-            qs = Item.objects.filter(self.select_pack(packs))
-            items = self.select_sort(sort, qs)
-            return items
-        qs = Item.objects.filter(self.select_pack(packs) & Q(third_category__name = category))
-        items = self.select_sort(sort, qs)
+            items = list(qs.order_by('price'))
         return items
     
-    def select_pack(self, packs):
-        queries = [Q(title__icontains=pack) if pack != '파우더' else Q(fourth_category__name = pack) for pack in packs]
-        query = queries.pop()
-        for each_q in queries:
-            query |= each_q
-        return query
+    def get_selected_items(self, sort, category, packs):
+        category_q = None
+        pack_q = None
+        if category:
+            category_q = Q(third_category__name=category) 
+        else:
+            category_q = Q(sub_category__name='Tea Shop')
+        if packs:
+            queries = [Q(title__icontains=pack) if pack != '파우더' else Q(fourth_category__name = pack) for pack in packs]
+            pack_q = queries.pop()
+            for each_q in queries:
+                pack_q |= each_q
+        else:
+            pack_q = Q(sub_category__name='Tea Shop')
+        return self.select_sort(sort, Item.objects.filter(category_q & pack_q))
 
     def get(self, request):
         sort = request.GET.get('sort', 'review')
-        category = request.GET.get('category', 'all')
-        packs = request.GET.getlist('pack', ['all'])
+        category = request.GET.get('category', None)
+        packs = request.GET.getlist('pack', None)
         page = int(request.GET.get('p', '0'))
-
-        items = self.evaluate_items(sort, category, packs)
-
-        num_pages = len(items)/self.OFFSET_PAGE + 1
+        
+        items = self.get_selected_items(sort, category, packs)
+        num_pages = len(items)//self.ITEMS_IN_PAGE + 1 if len(items)%self.ITEMS_IN_PAGE != 0 else len(items)//self.ITEMS_IN_PAGE
         item_values = []
-        for i in range(self.OFFSET_PAGE * page, self.OFFSET_PAGE + (self.OFFSET_PAGE * page)):
-            if i >= len(items) - 1:
-                 return JsonResponse({'items':item_values}, status=200)            
+        for i in range(self.ITEMS_IN_PAGE * page, self.ITEMS_IN_PAGE + (self.ITEMS_IN_PAGE * page)):
+            if i > len(items) - 1:
+                return JsonResponse({'items':item_values, 'num_pages':num_pages}, status=200)            
             label_dict = items[i].get_labels()
             item = {
-                 'id' : items[i].id,
-                 'title' : items[i].title,
-                 'price' : items[i].price,
-                 'discount_percent' : items[i].discount_percent,
-                 'num_reviews' : items[i].itemreview_set.count(),
-                 'num_wishlist' : items[i].wishlisted_users.count(),
-                 'best' : label_dict['BEST'],
-                 'gift' : label_dict['선물용'],
-                 'sold_out' : label_dict['일시품절'],
-                 'on_sale' : label_dict['SALE'],
-                 'bonus' : label_dict['사은품'],
-                 'new' : label_dict['NEW'],
-                 'front' : items[i].images.front_url,
-                 'hover' : items[i].images.hover_url,
+                'id' : items[i].id,
+                'title' : items[i].title,
+                'price' : items[i].price,
+                'discount_percent' : items[i].discount_percent,
+                'num_reviews' : items[i].itemreview_set.count(),
+                'num_wishlist' : items[i].wishlisted_users.count(),
+                'best' : label_dict['BEST'],
+                'gift' : label_dict['선물용'],
+                'sold_out' : label_dict['일시품절'],
+                'on_sale' : label_dict['SALE'],
+                'bonus' : label_dict['사은품'],
+                'new' : label_dict['NEW'],
+                'front' : items[i].images.front_url,
+                'hover' : items[i].images.hover_url,
             }
             item_values.append(item)
         return JsonResponse({'items':item_values, 'num_pages':num_pages}, status=200)
@@ -91,6 +82,7 @@ class ItemDetailView(View):
         if item.itemreview_set.all():
             rating_dict = item.itemreview_set.all().aggregate(Avg('overall_rating'))
             rating = rating_dict["overall_rating__avg"]
+        f_rating = f'{rating:.1f}'
 
         item_dict = {
             'sub_category' : item.sub_category.name,
@@ -106,7 +98,7 @@ class ItemDetailView(View):
             'bonus' : label_dict['사은품'],
             'new' : label_dict['NEW'],
             'benefits' : item.get_benefits(),
-            'rating' : rating,
+            'rating' : f_rating,
             'num_reviews' : item.itemreview_set.count(),
             'main_image' : item.images.main_url
         }
