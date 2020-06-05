804c0c3c593d31bbd4c8edce8e8651bb689188c4
sihwan0Lee <stay136@gmail.com>
2020-06-05 13:19:07 +0900

    [Fix]피드백수정 -review views ㄱ간격수정 -store views 주석삭제 -store views 클래스이름변경 -review views 클래스 통합 -전체적인 뛰어쓰기 간격조정

--- review/urls.py
+++ review/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import ReviewWriteView, ReviewView, ReviewDetailView
 
 urlpatterns = [
-    path('/write', ReviewWriteView.as_view()),
     path('' , ReviewView.as_view()),
     path('/detail/<int:review_id>', ReviewDetailView.as_view()),
 
--- review/views.py
+++ review/views.py
@@ -12,22 +12,21 @@ from .models import(
 from order.models import Order
 from aboutteatime.utils import logindecorator
 
-class ReviewWriteView(View):
+class ReviewView(View):
     @logindecorator
     def post(self, request):
         data = json.loads(request.body)
         item_id_from_front = data['item_id_from_front']
         if Item.objects.filter(id=item_id_from_front).exists() and Order.objects.filter(user_id=request.user.id).exists():
             ItemReview(
-                user_id         = request.user.id,
-                content         = data['content'],
-                item_id = data['item_id_from_front'],
-                overall_rating   = data['overall_rating'],
+                user_id        = request.user.id,
+                content        = data['content'],
+                item_id        = data['item_id_from_front'],
+                overall_rating = data['overall_rating'],
                 ).save()
             return HttpResponse(status=200)
         return JsonResponse({'comments':list(ItemReview.objects.values())}, status=200)
 
-class ReviewView(View):
     def get(self, request):
         item_id_from_front = request.GET.get('item_id_from_front')
         reviews = ItemReview.objects.filter(item_id=item_id_from_front)
@@ -44,20 +43,20 @@ class ReviewView(View):
 
 class ReviewDetailView(View):
     def get(self, request,id_from_front):
-        detailreviews = ItemReview.objects.filter(id = id_from_front)
+        reviews = ItemReview.objects.filter(id = id_from_front)
         detail_reviews=[]
-        for detailreview in detailreviews:
+        for review in reviews:
             desc_info={
-                    "itemname"    : detailreview.item.item,
-                    "username"    : detailreview.user.user,
-                    "delivery"    : detailreview.packaging_rating,
-                    "fragrance"   : detailreview.fragrance_rating,
-                    "taste"       : detailreview.taste_rating,
-                    "comment"     : detailreview.content,
-                    "overallrate" : detailreview.overall_rating,
-                    "date"        : detailreview.created_at,
-                    "like"        : detailreview.liked_by,
-                    "image"       : detailreview.image_url
+                    "itemname"    : review.item.item,
+                    "username"    : review.user.user,
+                    "delivery"    : review.packaging_rating,
+                    "fragrance"   : review.fragrance_rating,
+                    "taste"       : review.taste_rating,
+                    "comment"     : review.content,
+                    "overallrate" : review.overall_rating,
+                    "date"        : review.created_at,
+                    "like"        : review.liked_by,
+                    "image"       : review.image_url
                 }
             detail_reviews.append(desc_info)
-        return JsonResponse({'reviews':detailreviews}, status=200) 
+        return JsonResponse({'reviews':detail_reviews}, status=200) 
